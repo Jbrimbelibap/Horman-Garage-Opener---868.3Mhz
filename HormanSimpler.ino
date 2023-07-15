34aa06208f2b6463e775b1e8c4ab6b2cfa8ccd71
Léonard de Vinci <90109439+Jbrimbelibap@users.noreply.github.com>
2023-07-15 13:16:11 +0200

    Update HormanSimpler.ino

--- HormanSimpler.ino
+++ HormanSimpler.ino
@@ -2,17 +2,17 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-//0 is P0, 1 is P1, 2 is P2, etc. - unlike the analog inputs, for digital outputs the pin number matches.
+//0 is P0, 1 is P1, 2 is P2, We use P2 for our data pin. ~~ I don't think that you will need to adjust the pulse length
 #define  FS1000A_DATA_PIN    2
 #define pulse 471
 
 void setup() {                
   pinMode(FS1000A_DATA_PIN, OUTPUT);  
 
-  digitalWrite(FS1000A_DATA_PIN, HIGH);
+  digitalWrite(FS1000A_DATA_PIN, HIGH);  //The remote sends a wakeup call only once, it starts with a 73ms long transmission (The garage uses OOK - on off keying, the easiest modulation)
   delay(73);
 
-  digitalWrite(FS1000A_DATA_PIN, LOW);
+  digitalWrite(FS1000A_DATA_PIN, LOW); //It is followed by a 173ms long pause. Then we move on to repeating the code sequence
   delay(173);
 }
 
@@ -48,10 +48,10 @@ void send(char c)
 void loop() {
 
    // now the decoded pulse definition taken from Universal Radio Hacker tool and your keys
-  char sequence1[] = "11111111011010011110100001100011111111100011";
+  char sequence1[] = "11111111000110100111100011000111111111100011"; //put your code in
  
   uint8_t i;
-  digitalWrite(FS1000A_DATA_PIN, LOW);
+  digitalWrite(FS1000A_DATA_PIN, LOW); //This is the 12ms block of transmission at the start of the code. The remote itself repeats it before each actual code sequence.
   delayMicroseconds(pulse);
   digitalWrite(FS1000A_DATA_PIN, HIGH);
   delayMicroseconds(pulse*25.6);
